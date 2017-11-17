496da39d7885844e299c585eb0ec53b6aa7bf789
Mister Fili <mr.fili.c@gmail.com>
2017-11-16 22:11:10 -0500

    learn would ya

---
+++ WhatDidYouLearn/UseWhatYouKnow/userinput_howwasyourday.py
@@ -0,0 +1,12 @@
+#ask for daily response
+
+response = input('how was your day? ')
+
+
+if response == 'not good':
+    print(' sorry, sad face')
+else:
+	response2 = input('come on, you can say more than that! ')
+
+#print(response2{}.format(response))
+print('I knew you could more than \"{}\"'.format(response))
