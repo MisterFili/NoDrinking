a63d3f01a13cd8e63b1928765d7738e7b945db86
Mister Fili <mr.fili.c@gmail.com>
2017-11-18 16:13:49 -0500

    updating basic shit

--- 02oddOReven.py
+++ 02oddOReven.py
@@ -1,12 +1,20 @@
 #ask the user for input. Depending on wether the number is even or odd, print out an appropriate message to the user
 #HINT: how do even/odd numbers react when divided by 2
 
+#assing the users number to 'usernum'
 usernum = int(input("Give me a number. lets see if its even or odd 	: "))
+#assign the 'check' to the number the user pics
 check = int(input('Give me a number to divide by:'))
 
+#if number is mod by 4
 if usernum % 4 == 0:
 	print(usernum, "is a multiple of 4")
 elif usernum % 2 == 0:
 	print(usernum, 'is even')
 else:
 	print(usernum, 'is an odd number')
+
+if usernum % check == 0:
+	print(usernum, " divides evenly by ", check)
+else:
+	print(usernum, 'does not divide evenly into', check)
--- WhatDidYouLearn/UseWhatYouKnow/userinput_howwasyourday.py
+++ WhatDidYouLearn/UseWhatYouKnow/userinput_howwasyourday.py
@@ -2,10 +2,12 @@
 
 response = input('how was your day? ')
 
+#next level part of the program.
+#if respose is shorter than 7 characters ask for more input
 
 if response == 'not good':
     print(' sorry, sad face')
 else:
 	response2 = input('come on, you can say more than that! ')
 
-print('I knew you could more than \"{}\"'.format(response))
+print('See, I knew you could\'ve said  more than \"{}\"'.format(response))
