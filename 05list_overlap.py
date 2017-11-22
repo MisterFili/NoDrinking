d78de775a834d7b93fd094c970d55ff5cb7e3041
Mister Fili <mr.fili.c@gmail.com>
2017-11-22 16:18:37 -0500

    pushing exercise 05

--- 05list_overlap.py
+++ 05list_overlap.py
@@ -1,6 +1,8 @@
 #listOverlap
-#  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
- # b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+'''
+write a program that returns a list that contains only the 
+elements that are common between the lists (without duplicates). 
+Make sure your program works on two lists of different sizes.
 
 class_attendance = ['me', 'myself', 'and', 'eye']
 
@@ -9,3 +11,15 @@ if name in class_attendance:
 	print(' {}, this person is enrolled'.format(name))
 else:
 	print('{}, did not show up to class'.format(name))
+'''
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+d = [random.randint(1,80)]
+
+
+for item in a:
+	if item in b and item not in c:
+		c.append(item)
+			
+print(c)
---
+++ 05p_list_overlap.py
@@ -0,0 +1,20 @@
+'''
+write a program that returns a list that contains only the 
+elements that are common between the lists (without duplicates). 
+Make sure your program works on two lists of different sizes.
+'''
+class_attendance = ['me', 'myself', 'and', 'eye']
+import random
+
+def main():
+	a = [5,2,6,8,3,45,2,34,34,4523,]
+	b = [3,4,2,34,41,34,65,34,2,346,2,3,]
+	c = []
+	print(a)
+
+	for item in a:
+		if item in b and a:
+			c.append(c)
+     print(c)
+if __name__ == "__main__":
+	main()
