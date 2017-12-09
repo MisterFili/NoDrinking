db2c3f9482db0265fb5b843b54176c0be9cddd97
Mister Fili <mr.fili.c@gmail.com>
2017-12-08 22:04:09 -0500

    leftovers

---
+++ WhatDidYouLearn/underscore_main/listpractice.py
@@ -0,0 +1,7 @@
+#exercise7 practice
+
+year_of_birth  = [1988, 1978, 1999, 1997, 1990]
+age = []
+
+for year in year_of_birth:
+    age.append(2017 - year)
---
+++ WhatDidYouLearn/underscore_main/loopydo.py
@@ -0,0 +1,12 @@
+#loop practice 
+#while 
+# i = 0
+# nums = list()
+# a = int(input('How many times would you like to say \'Hello\' :'))
+# while i < a:
+# 	nums.append(i)
+# 	print(nums)
+# 	i = i+1
+
+nums = list(range(4, 11, 2))
+print(nums)
