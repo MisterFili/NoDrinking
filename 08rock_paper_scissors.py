531febb06c55e6faad3796a880be9b70181df968
Mister Fili <mr.fili.c@gmail.com>
2018-01-15 00:00:05 -0500

    rock paper scissors

---
+++ 08rock_paper_scissors.py
@@ -0,0 +1,39 @@
+
+import sys
+"""
+Make a two-player Rock-Paper-Scissors game.
+(Hint: Ask for player plays (using input),
+compare them, print out a message of congratulations
+to the winner, and ask if the players want to start a new game)
+"""
+
+print('''Please pick one:
+            rock
+            scissors
+            paper''')
+
+while True:
+    game_dict = {'rock': 1, 'scissors': 2, 'paper': 3}
+    player_a = str(raw_input("Player a: "))
+    player_b = str(raw_input("Player b: "))
+    a = game_dict.get(player_a)
+    b = game_dict.get(player_b)
+    dif = a - b
+
+    if dif in [-1, 2]:
+        print('player a wins.')
+        if str(raw_input('Do you want to play another game, yes or no?\n')) == 'yes':
+            continue
+        else:
+            print('game over.')
+            break
+    elif dif in [-2, 1]:
+        print('player b wins.')
+        if str(raw_input('Do you want to play another game, yes or no?\n')) == 'yes':
+            continue
+        else:
+            print('game over.')
+            break
+    else:
+        print('Draw.Please continue.')
+        print('')
